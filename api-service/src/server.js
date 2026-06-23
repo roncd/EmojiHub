cf1b7d3e13f9d8afed04f1b8c53339b4f0d36c19
roncd <roncd@github.com>
2026-06-23 11:53:24 +0200

    fix: ajout yml pour doc api

--- api-service/src/server.js
+++ api-service/src/server.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
 const config = require("../swagger.config");
-
 const specs = swaggerJsdoc(config);
+
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.listen(PORT, () => {
   console.log(`api-service a démarré sur le port:  ${PORT}`)
