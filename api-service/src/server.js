6eefbe39066cfdbb0e030f55f3fe272618855c27
roncd <roncd@github.com>
2026-06-23 11:32:18 +0200

    feat: ajout des bloc @swagger

--- api-service/src/server.js
+++ api-service/src/server.js
@@ -1,9 +1,14 @@
-require('dotenv').config()
+require('dotenv').config();
 const app = require('./app')
 
 const PORT = process.env.PORT
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const config = require("../swagger.config");
 
+const specs = swaggerJsdoc(config);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.listen(PORT, () => {
   console.log(`api-service a démarré sur le port:  ${PORT}`)
 })
